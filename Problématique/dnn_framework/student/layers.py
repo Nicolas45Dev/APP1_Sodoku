ed3d615576dfe21c21782b13d50ab3446f8fd235
anthopop123 <anthony.59@live.ca>
2024-09-04 13:25:14 -0400

    add layers

--- Problématique/dnn_framework/student/layers.py
+++ Problématique/dnn_framework/student/layers.py
@@ -9,14 +9,10 @@ class FullyConnectedLayer(Layer):
     """
     w: np.ndarray
     b: np.ndarray
-    input: np.ndarray
-    output: np.ndarray
 
     def __init__(self, input_count, output_count):
-        self.input = np.zeros(input_count)
-        self.output = np.zeros(output_count)
-        self.w = np.zeros(input_count)
-        self.b = np.zeros(1)
+        self.w = np.random.randn(output_count,input_count) * 2/(input_count+output_count)
+        self.b = np.random.randn(output_count,) * 2/output_count
 
     def get_parameters(self):
         return {'w': self.w, 'b': self.b}
@@ -39,27 +35,65 @@ class BatchNormalization(Layer):
     """
     This class implements a batch normalization layer.
     """
-
+    alpha: float
+    gamma: np.ndarray
+    beta: np.ndarray
+    mean: np.ndarray
+    variance: np.ndarray
+    epsilon: float = 1e-8
     def __init__(self, input_count, alpha=0.1):
-        raise NotImplementedError()
-
+        self.alpha = alpha
+        self.gamma = np.zeros(input_count)
+        self.beta = np.zeros(input_count)
+        self.mean = np.zeros(input_count)
+        self.variance = np.ones(input_count)
+        Layer.train(self)
     def get_parameters(self):
-        raise NotImplementedError()
+        return {'gamma': self.gamma, 'beta': self.beta}
+
 
     def get_buffers(self):
-        raise NotImplementedError()
+        return {"global_mean":self.mean, "global_variance":self.variance}
 
     def forward(self, x):
-        raise NotImplementedError()
+        if self._is_training:
+            return self._forward_training(x)
+        else:
+            return self._forward_evaluation(x)
 
     def _forward_training(self, x):
-        raise NotImplementedError()
+        batch_mean = np.mean(x, axis=0)
+        batch_variance = np.var(x, axis=0)
+        self.mean = self.alpha * self.mean + (1 - self.alpha) * batch_mean
+        self.variance = self.alpha * self.variance + (1 - self.alpha) * batch_variance
+        x_norm = (x - batch_mean) / np.sqrt(batch_variance + self.epsilon)
+        out = self.gamma * x_norm + self.beta
+        return [out, x]
 
     def _forward_evaluation(self, x):
-        raise NotImplementedError()
+        batch_mean = self.mean
+        batch_variance = self.variance
+        x_norm = (x - batch_mean) / np.sqrt(batch_variance + self.epsilon)
+        out = self.gamma * x_norm + self.beta
 
-    def backward(self, output_grad, cache):
-        raise NotImplementedError()
+        return [out, x]
+
+    def backward(self, output_grad, x):
+        batch_mean = np.mean(x, axis=0)
+        batch_variance = np.var(x, axis=0)
+        x_norm = (x - batch_mean) / np.sqrt(batch_variance + self.epsilon)
+
+        dLdx_norm = output_grad * self.gamma
+        dLdvar = np.sum(dLdx_norm * (x-batch_mean) * -0.5 * np.power(batch_variance + self.epsilon, -1.5), axis=0)
+        dLdmean = - np.sum(dLdx_norm / np.sqrt(batch_variance + self.epsilon), axis=0)
+        dLdx = dLdx_norm / np.sqrt(batch_variance + self.epsilon) + (2 / x.shape[0]) * dLdvar * (x - batch_mean) + (dLdmean / x.shape[0])
+
+        dLdgamma = np.sum(output_grad * x_norm,axis=0)
+        dLdb = np.sum(output_grad,axis=0)
+
+        return [dLdx,{"gamma": dLdgamma, "beta": dLdb}]
+
+
 
 
 class Sigmoid(Layer):
@@ -75,10 +109,11 @@ class Sigmoid(Layer):
 
     def forward(self, x):
         result = 1 / (1 + np.exp(-x))
-        return result, result
+        return result, x
 
     def backward(self, output_grad, cache):
-        raise NotImplementedError()
+        y, _ = self.forward(cache)
+        return [(y - y ** 2) * output_grad, cache]
 
 
 class ReLU(Layer):
@@ -87,14 +122,16 @@ class ReLU(Layer):
     """
 
     def get_parameters(self):
-        raise NotImplementedError()
+        return ()
 
     def get_buffers(self):
+        # Quelques choses?
         raise NotImplementedError()
 
     def forward(self, x):
-        result = ( x >= 0).astype(float) * x
-        return result, result
+        y = ( x >= 0).astype(float) * x
+        return y, x
 
     def backward(self, output_grad, cache):
-        raise NotImplementedError()
+        y = ( cache >= 0).astype(float) * output_grad
+        return [y,cache]
