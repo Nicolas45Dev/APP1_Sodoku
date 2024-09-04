e9b2a2061c1cb5ad78569431169b913bf016ea2d
anthopop123 <anthony.59@live.ca>
2024-09-04 16:59:04 -0400

    update

--- Problématique/dnn_framework/student/optimizers.py
+++ Problématique/dnn_framework/student/optimizers.py
@@ -6,11 +6,11 @@ class SgdOptimizer(Optimizer):
     This class implements a stochastic gradient descent optimizer.
     """
     learning_rate: float
-    parameters: dict
+    _parameters: dict
 
     def __init__(self, parameters, learning_rate=0.01):
         self._parameters = parameters
         self.learning_rate = learning_rate
 
     def _step_parameter(self, parameter, parameter_grad, parameter_name):
-        self._parameters[parameter_name] = parameter - self.learning_rate * parameter_grad
+        return parameter - self.learning_rate * parameter_grad
