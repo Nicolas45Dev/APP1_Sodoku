4094a4fc0029384c5732b226e1fb09db24eafb05
nicolas <nico645@outlook.com>
2024-09-04 13:26:59 -0400

    edit losses and optimizers

--- Problématique/dnn_framework/student/losses.py
+++ Problématique/dnn_framework/student/losses.py
@@ -14,7 +14,18 @@ class CrossEntropyLoss(Loss):
         :param target: The target classes (shape: (N,))
         :return A tuple containing the loss and the gradient with respect to the input (loss, input_grad)
         """
-        raise NotImplementedError()
+        # The error is -Sum(p(x) * log(q(x))), where p(x) is the target distribution and q(x) is the predicted distribution
+        probabilities = softmax(x)
+        n_echantillons = x.shape[0]
+
+        p = -np.log(probabilities[range(n_echantillons), target])
+        loss = np.sum(p) / n_echantillons
+
+        input_grad = probabilities.copy()
+        input_grad[range(n_echantillons), target] -= 1
+        input_grad /= n_echantillons
+
+        return loss, input_grad
 
 
 def softmax(x):
@@ -22,7 +33,8 @@ def softmax(x):
     :param x: The input tensor (shape: (N, C))
     :return The softmax of x
     """
-    raise NotImplementedError()
+    exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+    return exp_x / np.sum(exp_x, axis=1, keepdims=True)
 
 
 class MeanSquaredErrorLoss(Loss):
@@ -36,4 +48,5 @@ class MeanSquaredErrorLoss(Loss):
         :param target: The target tensor (shape: same as x)
         :return A tuple containing the loss and the gradient with respect to the input (loss, input_grad)
         """
-        raise NotImplementedError()
+        error = np.mean((x - target) ** 2)
+        return error, 2 * (x - target) / x.size
